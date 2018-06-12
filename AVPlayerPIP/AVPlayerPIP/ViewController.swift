bcdcc3b3dd968bc8e8ceb1d6e9ac36dd8a214f2c
derrick <derrick.kang@nexstreaming.com>
2018-06-12 11:33:17 +0900

    added basic project

---
+++ AVPlayerPIP/AVPlayerPIP/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  AVPlayerPIP
+//
+//  Created by derrick on 12/06/2018.
+//  Copyright © 2018 derrick. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
